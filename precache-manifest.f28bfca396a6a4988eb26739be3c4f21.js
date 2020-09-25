83de808a2076c0cd7053ed64748cb2f15bc5b7b0
jonatanretama <jonatanretama@gmail.com>
2020-09-25 13:14:26 -0500

    Updates

---
+++ precache-manifest.f28bfca396a6a4988eb26739be3c4f21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1248761f782ae268f5f7e025e01a51c",
+    "url": "/calculate-credit-react/index.html"
+  },
+  {
+    "revision": "93b568dc12d31fe88f2e",
+    "url": "/calculate-credit-react/static/css/main.ea0b92df.chunk.css"
+  },
+  {
+    "revision": "467ced22d3933239ee3c",
+    "url": "/calculate-credit-react/static/js/2.be2f48be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculate-credit-react/static/js/2.be2f48be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b568dc12d31fe88f2e",
+    "url": "/calculate-credit-react/static/js/main.9acf4527.chunk.js"
+  },
+  {
+    "revision": "548c68091739b78c027c",
+    "url": "/calculate-credit-react/static/js/runtime-main.2472069f.js"
+  }
+]);
